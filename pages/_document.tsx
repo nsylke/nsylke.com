4b684f880e886c6ffa5f4e90af9a11db88dd5793
Nicholas Sylke <nicksylke@outlook.com>
2020-06-14 10:24:17 -0500

    refactor: replace normalize.css cdn with local copy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,32 +11,31 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <meta charSet="utf-8"/>
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-                    <meta name="description" content="Nicholas Sylke - Portfolio"/>
+                    <meta name="description" content="Nicholas Sylke - Portfolio" />
 
-                    <meta property="og:title" content="Nicholas Sylke"/>
-                    <meta property="og:description" content="Nicholas Sylke - Portfolio"/>
-                    <meta property="og:type" content="website"/>
+                    <meta property="og:title" content="Nicholas Sylke" />
+                    <meta property="og:description" content="Nicholas Sylke - Portfolio" />
+                    <meta property="og:type" content="website" />
 
-                    <meta name="twitter:card" className="summary"/>
-                    <meta name="twitter:creator" className="@nsylke"/>
-                    <meta name="twitter:title" className="Nicholas Sylke"/>
-                    <meta name="twitter:description" className="Nicholas Sylke - Portfolio"/>
+                    <meta name="twitter:card" className="summary" />
+                    <meta name="twitter:creator" className="@nsylke" />
+                    <meta name="twitter:title" className="Nicholas Sylke" />
+                    <meta name="twitter:description" className="Nicholas Sylke - Portfolio" />
 
-                    <link rel="dns-prefetch" href="https://cdn.jsdelivr.net"/>
-                    <link rel="dns-prefetch" href="https://fonts.googleapis.com"/>
+                    <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
 
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css"/>
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap"/>
+                    <link rel="stylesheet" href="css/normalize.css" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" />
 
-                    <script src="https://kit.fontawesome.com/491005fb35.js"/>
+                    <script src="https://kit.fontawesome.com/491005fb35.js" />
                 </Head>
                 <body>
-                    <Main/>
-                    <NextScript/>
+                    <Main />
+                    <NextScript />
                 </body>
             </Html>
         )
