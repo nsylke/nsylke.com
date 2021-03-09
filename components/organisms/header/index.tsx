a44a9b769e213cd211f26a8d1aa9b8e23161e04b
Nicholas Sylke <nicksylke@outlook.com>
2021-03-09 16:21:42 -0600

    feat: automatically calculate age from birth date

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -3,20 +3,26 @@ import Title from "../../atoms/title";
 import Subtitle from "../../atoms/subtitle";
 import SocialMediaContainer from "../../molecules/social-media-container";
 
-const Header: React.FC = () => (
-    <React.Fragment>
-        <header>
-            <Title text="Nicholas Sylke"/>
-            <Subtitle text="22 year old software engineer working at Ken Cook currently."/>
-            <SocialMediaContainer/>
-        </header>
-        {/* language=CSS */}
-        <style jsx>{`
-            header {
-                padding-top: 130px;
-            }
-        `}</style>
-    </React.Fragment>
-)
+const Header: React.FC = () => {
+    const getAge = (time: string) => {
+        return Math.floor((new Date().getTime() - new Date(time).getTime()) / 3.15576e+10);
+    }
+
+    return (
+        <React.Fragment>
+            <header>
+                <Title text="Nicholas Sylke"/>
+                <Subtitle text={`${getAge(process.env.BIRTHDAY!)} year old software engineer.`}/>
+                <SocialMediaContainer/>
+            </header>
+            {/* language=CSS */}
+            <style jsx>{`
+                header {
+                    padding-top: 130px;
+                }
+            `}</style>
+        </React.Fragment>
+    )
+}
 
 export default Header;
