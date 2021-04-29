ac032608a706b49bfc9be0dc67e880245acbc5ac
Nicholas Sylke <nicksylke@outlook.com>
2021-04-28 21:28:00 -0500

    refactor: add title back to homepage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,8 @@ class MyDocument extends Document {
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+                    <title>Nicholas Sylke - Portfolio</title>
+
                     <meta name="description" content="Nicholas Sylke - Portfolio" />
 
                     <meta property="og:title" content="Nicholas Sylke" />
