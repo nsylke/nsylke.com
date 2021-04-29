ba766cbe98ec590d67b109c52614e82459bbb910
Nicholas Sylke <nicksylke@outlook.com>
2021-04-28 21:27:40 -0500

    refactor: use new homepage by default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { NextPage } from 'next';
-import Head from 'next/head';
-import Content from '../components/Content';
+import Header from "../components/organisms/header";
 
-const Page: NextPage = () => (
+const Page = () => (
     <React.Fragment>
-        <Head>
-            <title>Nicholas Sylke — Software Engineer</title>
-        </Head>
-        <Content />
+        <div className="container">
+            <Header/>
+        </div>
         {/* language=CSS */}
         <style jsx global>{`
-            html {
-                height: 100%;
-            }
-
             body {
-                height: 100%;
-                background-color: #194271;
                 font-family: 'Inter', sans-serif;
                 font-size: 10px;
-                color: #FFFFFF;
                 -webkit-font-smoothing: antialiased;
                 -moz-osx-font-smoothing: grayscale;
+            }
+
+            .container {
+                max-width: 1440px;
+                padding: 0 100px;
+                margin: 0 auto;
             }
         `}</style>
     </React.Fragment>
