ca33ccc79889e50b3e04eabe9d4b500a97828180
Nicholas Sylke <nicksylke@outlook.com>
2021-04-28 21:24:01 -0500

    refactor: update copy for title and subtitle

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -4,15 +4,11 @@ import Subtitle from "../../atoms/subtitle";
 import SocialMediaContainer from "../../molecules/social-media-container";
 
 const Header: React.FC = () => {
-    const getAge = (time: string) => {
-        return Math.floor((new Date().getTime() - new Date(time).getTime()) / 3.15576e+10);
-    }
-
     return (
         <React.Fragment>
             <header>
-                <Title text="Nicholas Sylke"/>
-                <Subtitle text={`${getAge(process.env.BIRTHDAY!)} year old software engineer.`}/>
+                <Title text="Hi, I'm Nicholas Sylke"/>
+                <Subtitle text="I'm a software engineer with a passion for building beautiful websites and applications."/>
                 <SocialMediaContainer/>
             </header>
             {/* language=CSS */}
