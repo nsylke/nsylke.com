ea951af7c4b013ea627ad77106211b82f79840b6
Nicholas Sylke <nicksylke@outlook.com>
2022-02-28 18:10:56 -0600

    feat: career section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Intro from '@components/Intro';
+import Career from '@components/Career';
 
 const Page = () => (
-    <div className="container mx-auto pt-[8em]">
+    <div className="container mx-auto py-[8em]">
         <Intro />
+        <Career />
     </div>
 );
 
