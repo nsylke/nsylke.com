22d5236d002e76c76d67bc751b01f36869343241
Nicholas Sylke <nicksylke@outlook.com>
2022-03-22 20:30:55 -0500

    refactor: remove career section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Intro from '@components/Intro';
-import Career from '@components/Career';
 
 const Page = () => (
     <div className="container mx-auto py-[8em]">
         <Intro />
-        <Career />
     </div>
 );
 
