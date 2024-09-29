4bf9a1245d03958ed6660557455dd27c1e4b0e40
nsylke <nick@trpkit.com>
2024-09-29 16:59:41 -0500

    refactor: add publishedAt and draft options to blog posts

--- src/keystatic/keystatic.config.ts
+++ src/keystatic/keystatic.config.ts
@@ -10,10 +10,35 @@ export default config({
     posts: collection({
       label: "Posts",
       slugField: "title",
-      path: "src/content/posts/*",
+      path: "src/content/posts/**",
+      entryLayout: "content",
       format: { contentField: "content" },
+      columns: ["title", "publishedAt"],
       schema: {
-        title: fields.slug({ name: { label: "Title" } }),
+        title: fields.slug({
+          name: {
+            label: "Title",
+            validation: {
+              length: {
+                min: 1,
+              },
+            },
+          },
+        }),
+        publishedAt: fields.date({
+          label: "Published at",
+          defaultValue: {
+            kind: "today",
+          },
+          validation: {
+            isRequired: true,
+          },
+        }),
+        draft: fields.checkbox({
+          label: "Draft",
+          description: "Check this box to prevent this post from being published.",
+          defaultValue: true,
+        }),
         content: fields.markdoc({ label: "Content" }),
       },
     }),
