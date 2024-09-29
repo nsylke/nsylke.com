f78a35f50693059520f8385a8434d1c4cf940970
nsylke <nick@trpkit.com>
2024-09-29 16:46:03 -0500

    refactor: rewrite url to use forwarded host & proto

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,18 +1,41 @@
 import keystaticConfig, { showAdminUI } from "@/keystatic/keystatic.config";
 import { makeRouteHandler } from "@keystatic/next/route-handler";
 
-export const { POST, GET } = (() => {
-  function notFoundHandler() {
+const { GET: _GET, POST: _POST } = makeRouteHandler({ config: keystaticConfig });
+
+function rewriteUrl(request: Request) {
+  const forwardedHost = request.headers.get("x-forwarded-host");
+  const forwardedProto = request.headers.get("x-forwarded-proto");
+
+  if (forwardedHost && forwardedProto) {
+    const url = new URL(request.url);
+
+    url.hostname = forwardedHost;
+    url.protocol = forwardedProto;
+    url.port = "";
+
+    return new Request(url, request);
+  }
+
+  return request;
+}
+
+export function GET(request: Request) {
+  if (!showAdminUI) {
     return new Response(null, {
       status: 404,
     });
   }
 
+  return _GET(rewriteUrl(request));
+}
+
+export function POST(request: Request) {
   if (!showAdminUI) {
-    return { GET: notFoundHandler(), POST: notFoundHandler() };
+    return new Response(null, {
+      status: 404,
+    });
   }
 
-  return makeRouteHandler({
-    config: keystaticConfig,
-  });
-})();
+  return POST(rewriteUrl(request));
+}
